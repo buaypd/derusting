4b9ad0a8ae96c5ced8a03df5a927a6d40527499e
Buay Dak <buaypd@gmail.com>
2026-03-22 12:34:41 -0700

    Finished ListPractice

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,27 +1,49 @@
+import java.util.*;
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
+    ArrayList<String> letters = new ArrayList<>();
 
     // Add 3 elements to the list (OK to do one-by-one)
+    letters.add("a");
+    letters.add("b");
+    letters.add("c");
 
     // Print the element at index 1
+    System.out.println(letters.get(1));
 
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
+    letters.set(1, "z");
 
     // Insert a new element at index 0 (the length of the list will change)
+    letters.add(0, "d");
 
     // Check whether the list contains a certain string
+    if (letters.contains("a")) {
+      System.out.println("This letter is in the list.");
+    }
+    else {
+      System.out.println("this letter is NOT in the list.");
+    }
 
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
+    for (int i = 0; i < letters.size(); i++) {
+      String letter = letters.get(i);
+      System.out.println(letter);
+    }
 
     // Sort the list using the Collections library
+    Collections.sort(letters);
 
     // Iterate over the list using a for-each loop
     // Print each value on a second line
+    for (String letter : letters) {
+      System.out.println(letter);
+    }
 
     /*
      * Usage tip!
